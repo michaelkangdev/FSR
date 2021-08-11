f20db0faf36337733ab7491d8cad138ad0f6c21b
michaelkangdev <michaelkang9898@gmail.com>
2021-08-11 23:44:08 +1200

    sql connection working

--- api/__init__.py
+++ api/__init__.py
@@ -6,8 +6,11 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://dbmasteruser:1qwx?&6*LzLcB`f`x:&`*70,[]tySRS~@ls-17b750ad5f19ab09f87e981f000631ea8fa6b698.cicx1pbp9xut.ap-southeast-2.rds.amazonaws.com/dev2'
+    DATABASE_URI='mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(user='dbmasteruser', password='1qwx?&6*LzLcB`f`x:&`*70,[]tySRS~', server='ls-17b750ad5f19ab09f87e981f000631ea8fa6b698.cicx1pbp9xut.ap-southeast-2.rds.amazonaws.com', database='dev2')
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
     db.init_app(app)
 
     from .views import main
